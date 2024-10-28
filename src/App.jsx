4fc852e5231c83c26884e0ad43140fc337fd64ef
GeekAmita <amitasingh5661@gmail.com>
2024-10-28 12:56:34 +0530

    Added Pet Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { createRoot } from "react-dom/client";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
       <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" />
+      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
+      <SearchParams />
     </div>
   );
 };
