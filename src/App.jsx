afb586747f46c303c493e6513247a480b748cfc6
GeekAmita <amitasingh5661@gmail.com>
2024-11-21 22:23:32 +0530

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me</h1>
-      {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
-      <SearchParams />
+      <BrowserRouter>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
